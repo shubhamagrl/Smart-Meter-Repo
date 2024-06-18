08a2ebfa6003e9b774d179f0bac7ac6fb3b116cc
Shubham Agrawal <agrlshubham@gmail.com>
2024-06-19 00:01:07 +0530

    Added button click toast message

--- app/src/main/java/com/example/smartmeter/MainActivity.kt
+++ app/src/main/java/com/example/smartmeter/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.smartmeter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myButton = findViewById(R.id.button)
+        myButton.setOnClickListener{
+            Toast.makeText(this, "You clicked the button", Toast.LENGTH_LONG).show()
+        }
     }
 }
