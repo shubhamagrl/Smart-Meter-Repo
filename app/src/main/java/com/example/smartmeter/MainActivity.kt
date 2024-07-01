67c80f8531f5c37231d9200186ea529e11ef23e8
shubhamagrl <s_agrawal@ph.iitr.ac.in>
2024-07-01 11:24:05 +0530

    Added some pic resources

--- app/src/main/java/com/example/smartmeter/MainActivity.kt
+++ app/src/main/java/com/example/smartmeter/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.smartmeter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var myButton : Button
+    lateinit var mytext : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         myButton = findViewById(R.id.button)
         myButton.setOnClickListener{
+
+            mytext.setText("Hello Shubham")
             Toast.makeText(this, "You clicked the button", Toast.LENGTH_LONG).show()
         }
     }
